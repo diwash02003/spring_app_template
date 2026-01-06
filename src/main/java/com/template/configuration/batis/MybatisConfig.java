0c95d99475575286938cc458122e4698808c9486
diwash02003 <pdiwash111@gmail.com>
2026-01-06 10:51:19 +0545

    feat: add MyBatis configuration

---
+++ src/main/java/com/template/configuration/batis/MybatisConfig.java
@@ -0,0 +1,39 @@
+package com.template.configuration.batis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @author diwash
+ * @created 1/4/26
+ */
+
+@Configuration
+@MapperScan("com.template.feature.**.mapper")
+public class MybatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+
+        factoryBean.setMapperLocations(
+                new PathMatchingResourcePatternResolver()
+                        .getResources("classpath*:mapper/**/*.xml")
+        );
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
